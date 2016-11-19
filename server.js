44ab4e3a572536345f6e23e8962ed4a03ba94f32
Monish Raj <rajwithgilly97@gmail.com>
2016-11-19 18:16:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,7 +49,8 @@ function createTemplate (data) {
               <div>
                   ${date.toDateString()}
               </div>
-              
+              <div id="likebutton">
+              </div>
               <div>
                 ${content}
               </div>
