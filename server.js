45ac25e8b94deabf2c31ffb96a085417fa56cbc8
Monish Raj <rajwithgilly97@gmail.com>
2016-11-26 22:50:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -173,9 +173,10 @@ app.get('/check-login', function (req, res) {
            if (err) {
               res.status(500).send(err.toString());
            } else {
-              res.send(result.rows[0].username);    
+              res.send(result.rows[0]);    
            }
        });
+       
    } else {
        res.status(400).send('You are not logged in');
    }
