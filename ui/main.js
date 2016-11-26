8bb7495b3cc7033c87e55ffc3c3937b0f646311d
Monish Raj <rajwithgilly97@gmail.com>
2016-11-26 23:22:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -87,7 +87,6 @@ function loadLoginForm () {
 }
 
 function loadLoggedInUser (userin) {
-    alert($userin);
     var user = JSON.parse(userin);
     var temp=user.username;
     var ilink=user.link;
